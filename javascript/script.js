9ccb692297c1b949f7c3618d8991f0a2288caa73
Shiroe <shirbhooshan1@gmail.com>
2026-03-11 20:46:13 +0545

    Adding Login form validation JS

--- javascript/script.js
+++ javascript/script.js
@@ -18,3 +18,25 @@ function addCart() {
 function passwordAlert() {
     alert("Please contact support to reset your password.");
 }
+
+//Login page validation
+loginForm = document.getElementById('LoginForm');
+
+    loginForm.addEventListener('submit', function (e) {
+      e.preventDefault();
+
+      const phone = document.getElementById('phone').value;
+      const password = document.getElementById('password').value;
+
+      const phonePattern = /^\d{10}$/;
+      if (!phonePattern.test(phone)) {
+        alert('Phone number must be exactly 10 digits (numbers only).');
+        return;
+      }
+
+      // Sending it to console for debugging
+      console.log('Logging in user:', { phone, password });
+
+      alert(`Login successful for ${phone}!`);
+    });
+    
